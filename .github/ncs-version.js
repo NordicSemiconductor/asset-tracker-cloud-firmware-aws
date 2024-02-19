c5970f879d2210e36dc8e943a1c84f3cdcdeacb0
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-19 16:10:04 +0100

    style: migrate to @bifravst/prettier-config

---
+++ .github/ncs-version.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import yaml from 'js-yaml'
+import path from 'path'
+
+const westYaml = yaml.load(
+	fs.readFileSync(path.join(process.cwd(), 'west.yml'), 'utf-8'),
+)
+
+process.stdout.write(
+	westYaml.manifest.projects.find(({ name }) => name === 'sdk-nrf').revision,
+)
